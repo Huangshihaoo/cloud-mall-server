5e3a2ead36814f4e726844e453c861e36693d701
haoo <84605859@qq.com>
2023-06-12 17:40:40 +0800

    共享模块

--- src/web/web.module.ts
+++ src/web/web.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GoodsController } from './goods/goods.controller';
 import { GoodsService } from './goods/goods.service';
 @Module({
+  imports: [GoodsService],
   controllers: [GoodsController],
   providers: [GoodsService],
   exports: [GoodsService],
